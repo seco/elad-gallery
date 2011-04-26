92da1b0ae805f662cf272812d70d8a051860a352
Elad Alfassa <elad@fedoraproject.org>
2011-04-26 17:19:00 +0300

    Added forgotten Hebrew translation

---
+++ locale/he.php
@@ -0,0 +1,29 @@
+<?
+$lang = array (
+	'File name:' => 'שם הקובץ:',
+	'File size:' => 'גודל הקובץ',
+	'Creation date:' => 'תאריך צילום:',
+	'Camera maker:' => 'יצרן המצלמה:',
+	'Camera model:' => 'סוג המצלמה:',
+	'Dimensions:' => 'ממדים:',
+	'seconds' => 'שניות',
+	'F number:' => 'מספר F:',
+	'Settings' => 'הגדרות',
+	'No' => 'לא',
+	'High quality preview' => 'תצוגה מקדימה באיכות גבוהה',
+	'Default: disabled' => 'ברירת מחדל: מנוטרל',
+	'low performance' => 'ביצועים נמוכים',
+	'High quality preview improves the preview quality in the info window, by downloading a full version of the image from the server (instead of a scaled-down version)and scaling it down in-browser. Using this feature will allow to zoom in and out. This is not recommended for slow connections. High quality preview is always enabled for files smaller than 1 MiB.' => 'תצוגה מקדימה באיכות גבוהה משפרת את איכות התצוגה המקדימה של התמונות בחלונית המידע, ע"י הורדת כל התמונה מהשרת (במקום גרסה מוקטנת שלה) והקטנתה בדפדפן. שימוש בפיצ\'ר הזה יאפשר להתקרב ולהתרחק (Zoom) מהתמונה. לא מומלץ לבעלי אינטרנט איטי. איכות גבוהה מופעלת באופן אוטומטי לקבצים שגודלם קטן מ־1 MiB.',
+	'Hash link to the info window' => 'קישור לחלונית המידע עם # בכתובת',
+	'Default: enabled' => 'ברירת מחדל: מופעל',
+	'comfortable' => 'נוח',
+	'Hash link to the info window changes the address add add a picture ID to it when opening the info window. Using this feature will allow copying a link to open the page and the info window and sending it to friends to point them to a specific picture, but might flood the browser history with entry for every picture viewed.' => 'קישור לחלונית המידע עם # משנה את שורת הכתובות ומוסיף לה את מזהה התמונה כשפותחים תמונה בחלונית המידע. שימוש בפיצ\'ר הזה יאפשר העתקת קישור שיפתח את הדף ואת חלונית המידע ושליחתו לחברים כדי להצביע להם על תמונה ספציפית, אבל עלול להציף את ההיסטוריה של הדפדפן בערכים על כל התמונות שנצפו.',
+	'Zoom in' => 'התקרבות',
+	'Zoom out' => 'התרחקות',
+	'Next picture' => 'התמונה הקודמת',
+	'Previous picture' => 'התמונה הבאה',
+	'Hide metadata' => 'הגדלת התמונה לתפיסת כל חלונית המידע',
+	'*Available in high quality preview only' => '*במצב איכות גבוהה בלבד',
+	'Keyboard shortcuts' => 'קיצורי מקשים'
+);
+?>
