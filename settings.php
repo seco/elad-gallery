c560b2bedbf6ed3d39bc86fb889481d31172fd41
Elad Alfassa <elad@fedoraproject.org>
2011-06-05 08:41:27 +0300

    really add settings.php this time

---
+++ settings.php
@@ -0,0 +1,4 @@
+<?
+	define('SCRIPT_DIR_URL', "http://www.doom.co.il/elad/pics/"); //URL to the directory where Gallery.php is.
+	define('IS_DIR_INDEX', true); //set to true if you have set DirectoryIndex Gallery.php in your .htaccess file.
+?>
