cb41b64f0d98f93a2ac6e1a9ea487150e558667b
Elad Alfassa <elad@fedoraproject.org>
2011-08-08 11:14:28 +0300

    Add copyright header to gallery.js and gallery.css

--- gallery.js
+++ gallery.js
@@ -1,3 +1,22 @@
+/*
+elad-gallery is a free, open sourced, lightweight and fast gallery that utilizes PHP, CSS3 and HTML5.
+	Copyright (C) 2010-2011  Elad Alfassa <elad@fedoraproject.org>
+
+	This file is part of elad-gallery.
+
+	elad-gallery is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	elad-gallery is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with elad-gallery. If not, see <http://www.gnu.org/licenses/>.
+*/
 var inhibitHashChange=false, hashTimeout;
 var dir="@@";
 var ScriptURI;
