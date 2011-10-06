1ffde7554fa40a38f8a8bf7793d528f7cd0a7b36
Elad Alfassa <elad@fedoraproject.org>
2011-10-06 16:37:58 +0200

    Add an option to configure some client-side features with a config file
    Right now this includes using Ajax and using transitions between images in the info window.

---
+++ internals/js/config.js
@@ -0,0 +1,18 @@
+/* This is a config file for the gallery.
+ * This file is where you change client side settings.
+ * More options will be added in the future, 
+ * including overriding other defaults.
+*/
+
+/* Default:
+config = {
+	useAnimations: true,
+	useAjax: true;
+  };
+
+*/
+
+config = {
+	useAnimations: true,
+	useAjax: true;
+  };
